342dd39c1c63d16be5e3e591167d448e4212d746
Rustam Salimov <rustam2027@yandex.ru>
2023-02-14 17:53:31 +0700

    add comments about complexity of division

--- divide.py
+++ divide.py
@@ -1,21 +1,21 @@
-def divide(x, y):
+def divide(x, y): # Пусть длина x = n, длина y = m (в двоичной системе), худшем случаем будет: x = 11...1; y = 10...0
     answer = 0
 
     if y == 0:
         raise ZeroDivisionError
 
-    while x and x >= y:
+    while x and x >= y: # Будет выполнено n + 1 раз
         temp_x = x
         counter = 0
 
-        while (temp_x >> 1) > y:
+        while (temp_x >> 1) > y: # Будет выполнено n - m раз
             counter += 1
             temp_x = temp_x >> 1
 
-        x = x - y * (2 ** counter)
+        x = x - y * (2 ** counter) # На вычитания потребуется n операций
         answer += 2 ** counter
 
-    return answer
+    return answer # общее колличество операций: (n + 1) * (n - m + n)
     
 for i in range(1, 5000):
     for j in range(1, 5000):
--- karasuba/make_new_tests.py
+++ karasuba/make_new_tests.py
@@ -3,5 +3,5 @@ from random import randint
 
 with open('karasuba/tests.txt', 'w') as file:
     for i in range(20):
-        file.write(str(randint(1, 9) * 10**10 + randint(10**9, 10**10)) + ' ' + str(10**11 + randint(10**9, 10**10)))
+        file.write(str(randint(1, 9) * 10**20 + randint(10**19, 10**20)) + ' ' + str(10**20 + randint(10**19, 10**20)))
         file.write('\n')
