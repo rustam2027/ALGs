623828dd136283292c1a69f5fd0ed146edae81bd
rustam2027 <109891738+rustam2027@users.noreply.github.com>
2023-02-14 10:34:17 +0700

    Remake karasuba

--- karasuba/karasuba.py
+++ karasuba/karasuba.py
@@ -1,29 +1,33 @@
+from math import floor, log
+
+
+def count_len(x):
+    counter = 0
+    while x:
+        x //= 10
+        counter += 1
+    return counter
+
+
 def mult(x, y):
-    x = str(x)
-    y = str(y)
-    
-    N = max(len(x), len(y))
-    
-    if N == 1:
+
+    N = max(count_len(x), count_len(y))
+
+    if N <= 2:
         return int(x) * int(y)
-    
+
     if N % 2:
         N += 1
-        
-    if len(x) < N:
-        x = '0' * (N - len(x)) + x
-    if len(y) < N:
-        y = '0' * (N - len(y)) + y
-    
-    a = x[0: N // 2]
-    b = x[N // 2: ]
-    c = y[0: N // 2]
-    d = y[N // 2: ]
-    
+
+    a = x // (10 ** (N // 2))
+    b = x % (10 ** (N // 2))
+    c = y // (10 ** (N // 2))
+    d = y % (10 ** (N // 2))
+
     a1 = mult(a, c)
     a2 = mult(b, d)
     a3 = mult(int(a) + int(b), int(c) + int(d)) - a2 - a1
-    
+
     return (a1 * (10 ** N)) + a2 + (a3 * (10 ** (N // 2)))
 
     
