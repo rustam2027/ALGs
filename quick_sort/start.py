b75c873d4cd921bacd475778f5f4c830a4d54e51
rustam2027 <109891738+rustam2027@users.noreply.github.com>
2023-05-22 18:23:34 +0700

    Update start.py

--- quick_sort/start.py
+++ quick_sort/start.py
@@ -1,25 +1,36 @@
 import subprocess as sp
 from random import randint
 import os
+import sys
 
 MIN_SIZE = 15
 MAX_SIZE = 16
 NUMBER_OF_ITERATIONS = 6
 
 files = ["branchfree_lomuto",
-         "standart_hoare",
-         "standart_lomuto"]
+         "standard_hoare",
+         "standard_lomuto"]
 
-test_path = "test"
+TEST_PATH = "test"
 
-output_path = "out.txt"
+OUTPUT_PATH = "out.txt"
 
-for file in files:
-    sp.run(["gcc", f"{file}.c", "-o", file, "-Ofast"])
+try:
+    os.chdir("/Users/rustamsalimov/Documents/GitHub/ALGs/quick_sort")
+
+    for file in files:
+        sp.run(["gcc", f"{file}.c", "-o", file, "-Ofast"], check=True, capture_output=False)
+except (FileNotFoundError, sp.CalledProcessError):
+    print("Error of finding file (files)")
+    sys.exit()
 
 array = {}
 
-os.chdir("/Users/rustamsalimov/Documents/GitHub/ALGs/quick_sort")
+
+def shuffle(nums: list):
+    for i in range(len(nums)):
+        j = randint(0, len(nums) - 1)
+        nums[i], nums[j] = nums[j], nums[i]
 
 
 def get_average(data: list) -> float:
@@ -49,31 +60,39 @@ def get_geometric_mean(data: list):
     return result
 
 
-for size in range(MIN_SIZE, MAX_SIZE):
-    for _ in range(NUMBER_OF_ITERATIONS):
-        with open(test_path, "w") as input:
-            for i in range(2 ** size):
-                input.write(f"{randint(1, 100)} ")
-            input.write("\n")
+try:
+    for size in range(MIN_SIZE, MAX_SIZE):
+        for _ in range(NUMBER_OF_ITERATIONS):
+            with open(TEST_PATH, "w", encoding='utf-8') as inp:
+                print(f"Creating test file, size={2 ** size}")
+                A = list(range(2 ** size))
+                print("Shuffling file")
+                shuffle(A)
+                print(f"Writing in {TEST_PATH} file")
+                for i in range(2 ** size):
+                    inp.write(f"{A[i]} ")
+                inp.write("\n")
 
-        for file in files:
-            sp.run([f"./{file}", test_path])
-            with open(output_path, "r") as output:
-                time = float(output.readline().strip())
+            for file in files:
+                print(f"Running tests for {file}")
+                sp.run([f"./{file}", TEST_PATH], check=True, capture_output=True)
+                with open(OUTPUT_PATH, "r", encoding="utf-8") as output:
+                    time = float(output.readline().strip())
 
-            if file not in array:
-                array[file] = {}
+                if file not in array:
+                    array[file] = {}
 
-            if size not in array[file]:
-                array[file][size] = [time]
-            else:
-                array[file][size].append(time)
+                if size not in array[file]:
+                    array[file][size] = [time]
+                else:
+                    array[file][size].append(time)
 
-for file in array:
-    for size in array[file]:
-        array[file][size] = get_geometric_mean(array[file][size])
-
-for file in files:
-    sp.run(["rm", file])
-
-print(array)
+    for file in array:
+        for size in array[file]:
+            array[file][size] = get_geometric_mean(array[file][size])
+finally:
+    for file in files:
+        sp.run(["rm", file], check=False)
+    sp.run(["rm", "test"], check=False)
+    sp.run(["rm", "out.txt"], check=False)
+    print(array)
